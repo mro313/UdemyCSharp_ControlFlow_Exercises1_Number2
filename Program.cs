fe99f861a99ea5ad7dbd91ad7d5b35778d19325e
mro313 <36976869+mro313@users.noreply.github.com>
2020-11-05 16:50:01 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+// 2- Write a program which takes two numbers from the console and displays the maximum of the two.
+
+namespace UdemyCSharp_ControlFlow_Exercises1_Number2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int numberOne = 40;
+            int numberTwo = 8;
+
+            if (numberOne > numberTwo)
+            {
+                Console.WriteLine(numberOne);
+            }
+
+            else
+            {
+                Console.WriteLine(numberTwo);
+            }
+        }
+    }
+}
